24e4b14025c7b76959790c7b6d940c13c0f1e429
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-01-12 16:17:08 +0100

    adds first structure to web pages

---
+++ src/php/consulting.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rick Randy - Consulting</title>
+</head>
+<body>
+    <header class="header-wrapper">
+        <div class="logo-wrapper">
+            <img src="" alt="" srcset="">
+        </div>
+        <nav class="nav-wrapper">
+            <ul class="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Books</a></li>
+                <li><a href="#">workshops</a></li>
+                <li><a href="#">Consulting</a></li>
+            </ul>
+        </nav>
+        <div>
+            <a href="#">Login</a>
+            <a href="#">Sign Up</a>
+        </div>
+    </header>
+    <main class="content-wrapper">
+        <div class="consulting-info info-wrapper">
+            <div>
+                <h1>Your <span>wish</span> is my <span>command</span></h1>
+                <p>Over the past years I gained a lot of expertise in my department and customer service.
+                    I want to share this knowledge with every web developer who is willing to learn and get
+                    better in their field. You can either book the free package or full premium package.
+                    You can also customize a package for you business needs.</p>
+            </div>
+        </div>
+        <div>
+            <div class="card-wrapper">
+                <div>
+                    <h2>Free</h2>
+                    <p><span>$</span>0</p>
+                    <p>Free includes</p>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <a href="#">Request</a>
+                </div>
+            </div>
+            <div class="card-wrapper">
+                <div class="card-header">
+                    <p>Most popular</p>
+                </div>
+                <div>
+                    <h2>Premimum</h2>
+                    <p><span>$</span>400</p>
+                    <p>Premium includes</p>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <a href="#">Request</a>
+                </div>
+            </div>
+            <div class="card-wrapper">
+                <div>
+                    <h2>Custom</h2>
+                    <p>Design a custom package for your business needs</p>
+                    <p>Custom includes</p>
+                    <ul>
+                        <li></li>
+                        <li></li>
+                    </ul>
+                    <a href="#">Request</a>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer class="footer-wrapper">
+    </footer>
+</body>
+</html>
