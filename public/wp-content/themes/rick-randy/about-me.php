51c9a30deafa33fb6f2dcabdb4da02222112ec1c
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-01-12 16:40:15 +0100

    first build

---
+++ public/wp-content/themes/rick-randy/about-me.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Rick Randy - About me</title>
+</head>
+<body>
+    <header class="header-wrapper">
+        <div class="logo-wrapper">
+            <img src="" alt="" srcset="">
+        </div>
+        <nav class="nav-wrapper">
+            <ul class="menu">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">About Me</a></li>
+                <li><a href="#">Books</a></li>
+                <li><a href="#">workshops</a></li>
+                <li><a href="#">Consulting</a></li>
+            </ul>
+        </nav>
+        <div>
+            <a href="#">Login</a>
+            <a href="#">Sign Up</a>
+        </div>
+    </header>
+    <main class="content-wrapper">
+        <div class="heading">
+            <h1>About <span>me</span></h1>
+        </div>
+        <div class="content-grid">
+            <div class="grid-elem">
+                <img src="" alt="" srcset="">
+            </div>
+            <div class="grid-elem">
+                <div>
+                    <h2>Who<span>am I</span></h2>
+                    <div>
+                        <p>
+                            It all starts with a “Hello World”! Or for me it was a book about HTML I found in my local
+                            library when I was 15. Since then, my whole life has been dedicated to coding. I went to
+                            university and finished my Bachelor degree.
+                        </p>
+                        <p>
+                            While working for several companies I found my passion in teaching other people. I never
+                            thought of myself as a teacher who likes to educate people but ironically that is what I
+                            end up loving now.
+                        </p>
+                        <p>
+                            Five years ago, I started my book-series “Why you rightfully hate JavaScript”. Two years
+                            ago, I build up my YouTube channel, which has now over 150.000 subscribers.
+                        </p>
+                    </div>
+                    <a type="submit">My yotube</a>
+                </div>
+            </div>
+            <div class="grid-elem">
+                <img src="" alt="" srcset="">
+            </div>
+            <div class="grid-elem">
+                <div>
+                    <h2>Work<span>experience</span></h2>
+                    <div>
+                        <p>Over the last years of me working in this business I’ve worked with many companies from
+                            all over the world, like AP MEDIA in Hamburg, Utility in New York or Blink22 in Singapore.
+                        </p>
+                        <p>
+                            While working in several countries I was also able to meet lots of freelancers with whom I’m
+                            working until this day. Because of working in the international business, I was able to work
+                            for lots of companies from all over the world remotely too.
+                        </p>
+                        <p>
+                            I’m in the web development business
+                            for over 10 years now and a certified trainer for the past three years. Today I’m a senior web
+                            developer while offering workshops and doing consulting.
+                        </p>
+                    </div>
+                    <a type="submit">My Linkedin</a>
+                </div>
+            </div>
+        </div>
+    </main>
+    <footer class="footer-wrapper">
+    </footer>
+</body>
+</html>
