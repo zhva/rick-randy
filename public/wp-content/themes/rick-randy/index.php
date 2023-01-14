8925acbd47fa289b01febebf7afd89ddeb772103
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2023-01-15 00:04:27 +0100

    moves header to a separate php file

--- public/wp-content/themes/rick-randy/index.php
+++ public/wp-content/themes/rick-randy/index.php
@@ -17,29 +17,7 @@
 </head>
 <body>
 <?php wp_body_open();?>
-    <header class="header-wrapper">
-        <div class="logo-wrapper">
-            <img src="" alt="" srcset="">
-        </div>
-        <nav>
-            <div class="nav-left">
-                <img src="<?php echo get_template_directory_uri();?>/images/Logo.svg" alt="Logo" />
-            </div>
-            <div class="wrapper">
-            <div class="nav-center">
-                <a href="#">Home</a>
-                <a href="#">About Me</a>
-                <a href="#">Books</a>
-                <a href="#">Workshops</a>
-                <a href="#">Consulting</a>
-            </div>
-            <div class="nav-right">
-                <a href="#" class="button button-primary">Login</a>
-                <a href="#" class="button button-secondary">Sign Up</a>
-            </div>
-            </div>
-        </nav>
-    </header>
+    <?php get_header(); ?>
     <main class="content-wrapper">
         <section class="section-hero">
             <div class="hero-content-wrapper">
